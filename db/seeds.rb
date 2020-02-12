bffaa022d2d5fa99f93c0a727e92ad789ba0b3e1
stephane fersing <stephane.fersing@gmail.com>
2020-02-12 16:48:17 +0100

    done model, controller and view index show new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3)
+
+Flat.create!(
+  name: 'Light  Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 78,
+  number_of_guests: 26
+)
+
+Flat.create!(
+  name: 'Flat Paris',
+  address: '10  W9 1DT',
+  description: 'A lovely large kitchen and a beautiful conservatory',
+  price_per_night: 1178,
+  number_of_guests: 260
+)
+
+Flat.create!(
+  name: 'Flat amsterdam',
+  address: '10  W9 1DT',
+  description: 'A lovely large kitchen and a beautiful conservatory',
+  price_per_night: 1178,
+  number_of_guests: 260
+)
+
+Flat.create!(
+  name: 'Enjoy',
+  address: 'Le wagon',
+  description: 'A lovely train',
+  price_per_night: 1178,
+  number_of_guests: 260
+)
